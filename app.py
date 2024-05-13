c373c5266219dae076a9ca165a197a5b143fb4b8
doms1994 <164338459+doms1994@users.noreply.github.com>
2024-05-13 21:09:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+JOBS=[
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'Location' : 'London',
+        'salary' : '125.000 GBP'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Analyst',
+        'Location' : 'London',
+        'salary' : '125.000 GBP'
+    },
+    {
+    'id' : 3,
+    'title' : 'Tyre fitter',
+    'Location' : 'London',
+    'salary' : '125.000 GBP'
+    },
+    {
+    'id' : 4,
+    'title' : 'Full stack engineer',
+    'Location' : 'London',
+    'salary' : '125.000 GBP'
+    }
+]
 
 @app.route("/")
 
 def hello_world():
-    return 'hey'
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/jobs")
+
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
